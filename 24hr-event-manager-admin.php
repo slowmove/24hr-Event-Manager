dee15c439e9044e0306d5ff3ca06a12e85175cb2
Erik Johansson <pg-wannabe@hotmail.com>
2012-12-12 10:05:39 +0100

    Fixed some issues with WP 3.5

--- 24hr-event-manager-admin.php
+++ 24hr-event-manager-admin.php
@@ -8,16 +8,30 @@ function EventManager_add_page() {
 	add_submenu_page('Event Manager', 'Event Manager', 'manage_options', 'EventManager_list_events', 'EventManagerList');
     add_submenu_page(dirname(__file__), 'Skapa Event', 'Skapa Event', 'manage_options', 'EventManager_create_event', 'EventManagerCreate');
 	add_submenu_page(dirname(__file__), 'Redigera Event', 'Redigera Event', 'manage_options', 'EventManager_edit_event', 'EventManagerEdit');
-    
+
 	wp_enqueue_style('EventManagerAdminCss');
     wp_enqueue_style('jQueryUICSS');
-		
-	wp_enqueue_script('EventManagerModal', array('jquery'));
-	wp_enqueue_script('Placeholder', array('jquery'));
-    wp_enqueue_script('jqueryUI', array('jquery'));
-    wp_enqueue_script('jqueryUItime', array('jquery'));
+
     wp_enqueue_script('mainAdmin',  array('jquery'));
 	wp_localize_script('mainAdmin', 'eventusershandling', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+}
+
+add_action('admin_enqueue_scripts', 'add_scripts');
+function add_scripts()
+{
+	wp_enqueue_script('EventManagerModal', array('jquery'));
+	wp_enqueue_script('Placeholder', array('jquery'));
+
+	wp_enqueue_script('jquery-ui-core');
+	wp_enqueue_script('jquery-ui-widget');
+	wp_enqueue_script('jquery-ui-mouse');
+	wp_enqueue_script('jquery-ui-slider');
+	wp_enqueue_script('jquery-ui-dialog');
+	wp_enqueue_script('jquery-ui-button');
+	wp_enqueue_script('jquery-ui-datepicker');
+
+	wp_enqueue_script('jqueryUIdate');
+    wp_enqueue_script('jqueryUItime', array('jquery-ui-datepicker'));
 }
 
 /**
